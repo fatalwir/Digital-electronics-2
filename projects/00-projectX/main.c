04deb487d5c30695647005866a5a851bbd3a6182
lab661 <lab661@PC-449.urel.feec.vutbr.cz>
2019-12-04 11:18:17 +0100

    [Project] Completed sending measured values, displaying received text on LCD, added license.

--- projects/00-projectX/main.c
+++ projects/00-projectX/main.c
@@ -1,177 +1,111 @@
-/***********************************************************************
- * 
- * Analog-to-digital conversion with displaying result on LCD and 
- * transmitting via UART.
- * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+/*
+ * main.c
  *
- * Copyright (c) 2018-2019 Tomas Fryza
- * Dept. of Radio Electronics, Brno University of Technology, Czechia
- * This work is licensed under the terms of the MIT license.
+ *       Created on:  Nov 21, 2019
+ *  Last Updated on:  Dec 04, 2019
+ *          Authors:  Ondrej Kolar
+ *                    Vojtech Herbrych
+ *
+ * Copyright (c) 2019 Ondrej Kolar, Vojtech Herbrych
  * 
- **********************************************************************/
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 /* Includes ----------------------------------------------------------*/
-#include <stdio.h>   
-#include <stdlib.h>      // itoa() function
-#include <avr/io.h>
 #include <avr/interrupt.h>
 #include "timer.h"
 #include "lcd.h"
 #include "uart.h"
-#include "gpio.h"
-#include <util/delay.h>
 
-/* Typedef -----------------------------------------------------------*/
 /* Define ------------------------------------------------------------*/
-#define UART_BAUD_RATE 76800 //115200
-#define NO_MEAS_MODE  0
-#define U_MEAS_MODE  1
-#define I_MEAS_MODE  2
+#define UART_BAUD_RATE 76800
+#define NO_MEAS_MODE   0
+#define U_MEAS_MODE    1
+#define I_MEAS_MODE    2
 
 /* Variables ---------------------------------------------------------*/
-uint8_t  measuring = U_MEAS_MODE;
-uint16_t uval = 0;
-uint16_t ival = 0;
-uint8_t  cnt = 0;
-uint8_t  a = 0;
-//uint8_t i = 24;
-
-char string[17];
-
-char array[3];
-
-// uint8_t first = 2;
-// uint8_t uval_l = 0;
-// uint16_t uval_h = 400;
-// uint8_t ival_l = 6;
-// uint16_t ival_h = 824;
-
-char uart_string[24];
+uint8_t  measuring = U_MEAS_MODE;   // Current measuring mode
+uint16_t uval = 0;  // Raw voltage value
+uint16_t ival = 0;  // Raw current value
+uint8_t  cnt = 0;   // Counter for discarding invalid ADC conversions
+char buffer[3];     // Outgoing UART buffer
 
 /* Function prototypes -----------------------------------------------*/
 void startConversion(uint8_t pin);
-void sendLCD(void);
-void sendUART(char array);
-/* Functions ---------------------------------------------------------*/
+void sendUART(void);
+
 /* Main --------------------------------------------------------------*/
-/* Read ADC result and transmit via UART. */
 int main(void)
 {
-    /* External 5 V ADC reference selection
-    ADMUX |= _BV(REFS0);
-    ADMUX &= ~(_BV(REFS1));
-    */
-
-    // first = 2;
-    // uval_h = 400;
-    // ival_h = 830;
-    // uval_l = 0;
-    // ival_l = 6;
-
     // Internal 1,1 V ADC reference selection
     ADMUX |= _BV(REFS0)|_BV(REFS1);
     // Enabling ADC, interrupt, select prescaling by 128 (f=125kHz)
     ADCSRA |= _BV(ADEN)|_BV(ADPS2)|_BV(ADPS1)|_BV(ADPS0)|_BV(ADIE);
 
+    // Init LCD library
     lcd_init(LCD_DISP_ON);
 
-    // uint8_t customChars[] = {
-    //     //BUT logo
-	//     0b00000,
-    //     0b11000,
-    //     0b00111,
-    //     0b00110,
-    //     0b00100,
-    //     0b00100,
-    //     0b00000,
-    //     0b00000
-    // };
-
-    // lcd_command(1<<LCD_CGRAM);
-
-    // int i;
-
-    // for (i=0; i<8; i++) {
-    //     lcd_data(customChars[i]);
-    // }
-
-    // // LCD display
-    // lcd_clrscr();
-    // lcd_puts("LCD test ... OK");
-    // _delay_ms(1000);
-    // lcd_clrscr();
-    // lcd_puts("Initializing ...");
-    // lcd_gotoxy(0,1);
-    // lcd_puts("VUT FEKT 2019  ");
-    // lcd_putc(0);
-    // _delay_ms(2000);
-    // lcd_clrscr();
-    
-
-    //Timers
-    /* Timer0
-     * TODO: Timer0 clock source and enable overflow 
-     *       interrupt */
-    // TIM_config_prescaler(TIM0, TIM_PRESC_256);
-    // TIM_config_interrupt(TIM0, TIM_OVERFLOW_ENABLE);
-
-    /* Timer1
-     * TODO: Timer1 clock source and enable overflow 
-     *       interrupt */
-    TIM_config_prescaler(TIM1, TIM_PRESC_64);
-    TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
-
-    /* Timer2
-     * TODO: Configure Timer2 clock source and enable overflow 
-     *       interrupt */
+    // Timer2 - 0.5 ms
     TIM_config_prescaler(TIM2, TIM_PRESC_32);
     TIM_config_interrupt(TIM2, TIM_OVERFLOW_ENABLE);
 
     // UART: asynchronous, 8-bit data, no parity, 1-bit stop
     uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
     
-    uart_puts("UART testing\r\n");
-
-    
     // Enables interrupts by setting the global interrupt mask
     sei();
 
-    // Infinite loop
-    for (;;) {
+    // Infinite loop - reading UART
+    while (1) {
+        uint16_t in = uart_getc();
+        // If byte is received
+        if(in>>8 == 0) {
+            // If control byte is received
+            if((uint8_t)in%256 > 127) {
+                // If clear bit is set
+                if((in>>6)%2 == 1) {
+                    lcd_clrscr();
+                }
+                // Set received cursor position
+                lcd_gotoxy(in%16, ((in>>4)%4));
+            } else if((uint8_t)in%256 == 0) {
+                // Ending byte received
+            } else {
+                lcd_putc((char)(in%256));
+            }
+        }
     }
-    // Will never reach thisccept instructions
+
+    // Will never reach here
     return (0);
 }
 
 /* Interrupts --------------------------------------------------------*/
-/* Timer1 overflow interrupt routine.
- * Start LCD write and UART transmitting. */
-
-// ISR(TIMER0_OVF_vect)
-// {
-//     if (measuring == NO_MEAS_MODE) {
-//     //     cnt = 0;
-//         sendUART();
-//     }
-// }
-
-ISR(TIMER1_OVF_vect)
-{
-    sendLCD();
-    //sendUART();
-}
-
-/* Timer2 overflow interrupt routine.
- * Start ADC conversion. */
+// Timer2 overflow interrupt routine.
 ISR(TIMER2_OVF_vect)
 {
     startConversion(U_MEAS_MODE);
     measuring = U_MEAS_MODE;
 }
 
-/* -------------------------------------------------------------------*/
-/* ADC complete interrupt routine.
- * Update UART transmiter. */
+// ADC complete interrupt routine
 ISR(ADC_vect)
 {   
     switch(measuring){
@@ -195,94 +129,47 @@ ISR(ADC_vect)
             }
             if (cnt == 3){
                 ival = ADC;
+                sendUART();
                 measuring = NO_MEAS_MODE;
                 cnt = 0;
             }
             break;
         case NO_MEAS_MODE:
-            // array[0] = uval*128;
-            // lcd_gotoxy(0,1);
-            // lcd_putc(array[0]);
-            // array[1] = uval%128;
-            // lcd_gotoxy(5,1);
-            // lcd_putc(array[1]);
-            // array[2] = ival%128;
-            // lcd_gotoxy(10,1);
-            // lcd_putc(array[2]);
-            //sendUART(array[0]);
             break;
         default:
+            measuring = NO_MEAS_MODE;
             break;
     }
  
 }
 
+/* Functions ---------------------------------------------------------*/
 void startConversion(uint8_t mode) {
     switch(mode) {
         case U_MEAS_MODE:
             // Select pin PA1
             ADMUX |= _BV(MUX0);
-            ADMUX &= ~(_BV(MUX1));//&_BV(MUX2)&_BV(MUX3));
+            ADMUX &= ~(_BV(MUX1));
             break;
         case I_MEAS_MODE:
             // Select pin PA2
             ADMUX |= _BV(MUX1);
-            ADMUX &= ~(_BV(MUX0));//&_BV(MUX2)&_BV(MUX3));
+            ADMUX &= ~(_BV(MUX0));
             break;
         default:
             return;
     }
-    ADCSRA |= _BV(ADSC);    // Start conversion
+    // Start conversion
+    ADCSRA |= _BV(ADSC);
 }
 
-void sendLCD(void) {
-    lcd_clrscr();
-    itoa(uval, string, 10);
-    lcd_gotoxy(0, 0);
-    lcd_puts("U=");
-    lcd_puts(string);
-    lcd_puts("V;");
-    itoa(ival, string, 10);
-    lcd_gotoxy(10, 0);
-    lcd_puts("I=");
-    lcd_puts(string);
-    lcd_puts("A");
+void sendUART(void) {
+    buffer[1] = uval%128;
+    buffer[2] = ival%128;
+    buffer[0] = 0;
+    buffer[0] = 1<<7 | (uval>>7)<<3 | ival>>7;
 
-    array[1] = uval%128;
-    array[2] = ival%128;
-    array[0] = 0;
-    array[0] = 1<<7 | (uval>>7)<<3 | ival>>7;
-
-    //lcd_gotoxy(0,1);
-    //lcd_putc(array[0]);
-    //array[1] = uval%128;
-    //lcd_gotoxy(5,1);
-    //lcd_putc(array[1]);
-    //array[2] = ival%128;
-    //lcd_gotoxy(10,1);
-    //lcd_putc(array[2]);
-    uart_putc(array[0]);
-    uart_putc(array[1]);
-    uart_putc(array[2]);
-    return;
+    uart_putc(buffer[0]);
+    uart_putc(buffer[1]);
+    uart_putc(buffer[2]);
 }
-
-void sendUART(char array) {
-    uart_putc(array);
-    // uart_putc(array[0]);
-    // uart_putc(array[1]);
-    // uart_putc(array[2]);
-    return;
-}
-
-// for (i; i<3; i++) {
-    //     if (uart_string[i] == '\0') {
-    //         uart_string[i] = ' ';
-    //     }
-    // }
-
-    // itoa(first, uart_string, 16);
-    // itoa(uval_l, &uart_string[1], 16);
-    // itoa(ival_l, &uart_string[2], 16);
-    // itoa(uval_h, &uart_string[3], 16);
-    // itoa(ival_h, &uart_string[4], 16);
